402d13a04787f29b6c8ca8a27a1080fbdf70b3c0
max <156676928+CryptoMax-web@users.noreply.github.com>
2024-01-18 15:50:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def notify():
 
 
                       # create the text string
-            message = f'*Token transfer*: \nvalue: {value} *,* *{token_symbol}*'
+                   message = f'*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue: {value} *{token_symbol}* {token_address}'
 
             if token_symbol is not None and token_symbol not in ['USDT', 'USDC', 'WBTC', 'WETH','DAI', 'ETH'] and float(value) >= 1000 and value != 0:
                 # fix the bug: check if token_symbol is None before checking if it is in the list
