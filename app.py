3f541a8b8da3b80d801bbea230bc94f1a9afd41c
max <156676928+CryptoMax-web@users.noreply.github.com>
2024-01-18 18:33:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, Response
 from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -12,48 +13,48 @@ user_chat_id = os.environ['CHANNEL_ID']
 
 @app.route('/')
 def hello():
-    return 'Service for sending notifications to a telegram channel '
+  return 'Service for sending notifications to a telegram channel '
 
 @app.route('/notify', methods=['POST', 'GET'])
 def notify():
-    logs = request.json
-    if (len(logs) == 0):
-        print("Empty logs array received, skipping")
-    else:
-        print(logs)
-        category = ""
-        try:
-            category = logs['event']['activity'][0]['category']
-        except:
-            print("category not defined")
-        if logs['webhookId'] == os.environ['ALCHEMY_KEY'] and category == 'token':
-            # extract the necessary information
-            txhash = from_address = "[" + str(logs['event']['activity'][0]['hash']) + "](https://etherscan.io/tx/" + str(logs['event']['activity'][0]['hash']) + ")"
+  logs = request.json
+  if (len(logs) == 0):
+    print("Empty logs array received, skipping")
+  else:
+    print(logs)
+    category = ""
+    try:
+      category = logs['event']['activity'][0]['category']
+    except:
+      print("category not defined")
+    if logs['webhookId'] == os.environ['ALCHEMY_KEY'] and category == 'token':
+      # extract the necessary information
+      txhash = from_address = "[" + str(logs['event']['activity'][0]['hash']) + "](https://etherscan.io/tx/" + str(logs['event']['activity'][0]['hash']) + ")"
 
-            from_address = "[" + str(logs['event']['activity'][0]['fromAddress']) + "](https://etherscan.io/address/" + str(logs['event']['activity'][0]['fromAddress']) + "#tokentxns)"
-            to_address = "[" + str(logs['event']['activity'][0]['toAddress']) + "](https://etherscan.io/address/" + str(logs['event']['activity'][0]['toAddress']) + "#tokentxns)"
+      from_address = "[" + str(logs['event']['activity'][0]['fromAddress']) + "](https://etherscan.io/address/" + str(logs['event']['activity'][0]['fromAddress']) + "#tokentxns)"
+      to_address = "[" + str(logs['event']['activity'][0]['toAddress']) + "](https://etherscan.io/address/" + str(logs['event']['activity'][0]['toAddress']) + "#tokentxns)"
 
-            token_symbol = logs['event']['activity'][0]['asset']
-            token_address = "[" + str(logs['event']['activity'][0]['rawContract']['address']) + "](https://etherscan.io/address/" + str(logs['event']['activity'][0]['rawContract']['address']) + ")"
+      token_symbol = logs['event']['activity'][0]['asset']
+      token_address = "[" + str(logs['event']['activity'][0]['rawContract']['address']) + "](https://etherscan.io/address/" + str(logs['event']['activity'][0]['rawContract']['address']) + ")"
 
-            value = str(round(logs['event']['activity'][0]['value']))
+      value = str(round(logs['event']['activity'][0]['value']))
 
-            # create the text string
-            message = f'*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue:*;* {value} *;* *;**{token_symbol}**;* {token_address}'
-            if token_symbol is not None and token_symbol not in ['USDT', 'USDC', 'WBTC', 'WETH','DAI', 'ETH'] and float(value) >= 1000 and value != 0:
-                # fix the bug: check if token_symbol is None before checking if it is in the list
-                if token_symbol is not None:
-                    bot.send_message(chat_id=user_chat_id, text=message, parse_mode='MarkdownV2')
+      # create the text string
+      current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+      message = f'*Token transfer on *;*{current_date}*;* * \n{txhash}\nfrom {from_address} \nto {to_address}:\nvalue:*;* {value} *;* *;**{token_symbol}**;* {token_address}'
 
-    return Response(status=200)
+      if token_symbol is not None and token_symbol not in ['USDT', 'USDC', 'WBTC', 'WETH','DAI', 'ETH'] and float(value) >= 1000 and value != 0:
+        # fix the bug: check if token_symbol is None before checking if it is in the list
+        if token_symbol is not None:
+          bot.send_message(chat_id=user_chat_id, text=message, parse_mode='MarkdownV2')
+
+  return Response(status=200)
 
 updater = Updater(TELEGRAM_API_TOKEN)
 # Start the bot
 updater.start_polling()
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
 
-if __name__ == '__main__':
-    app.run()
 
